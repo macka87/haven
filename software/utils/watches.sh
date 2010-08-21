98fb0cf0ea3fd9b0ecd9b617e0f80df69333fc60
Ondra Lengal <ondra.lengal@gmail.com>
2010-08-21 13:50:35 +0000

    Watches util: change to nicer print-out
    git-svn-id: svn+ssh://merlin.fit.vutbr.cz/svn/DPI13@10 bda6da93-004a-4ae9-8e07-715c10848801

--- software/utils/watches.sh
+++ software/utils/watches.sh
@@ -1,11 +1,12 @@
 #!/bin/sh
 
+RAW_VAL=$(csbus 00080018)
+SRCDST=$(echo 'ibase=16;' $(echo ${RAW_VAL} | tr 'abcdef' 'ABCDEF') | bc)
+
 echo ""
 echo "Application core watches:"
-echo "Start/Stop register:            " $(csbus 00080000)
-echo "Input interface frame counter:  " $(csbus 00080008)
-echo "Output interface frame counter: " $(csbus 0008000C)
-echo "Input invalid frame counter:    " $(csbus 00080010)
-echo "Output invalid frame counter:   " $(csbus 00080014)
-echo "Interface ready signals:        " $(csbus 00080018)
+echo "Control register:            " $(csbus 00080000)
+echo ""
+echo "Input interface:    frames:  " $(csbus 00080008) "    invalid frames: " $(csbus 00080010) "   DST_RDY_N: " $(echo ${SRCDST} '/ (2^ 0) % 2' | bc ) " SRC_RDY_N: " $(echo ${SRCDST} '/ (2^ 1) % 2' | bc )
+echo "Output interface:   frames:  " $(csbus 0008000C) "    invalid frames: " $(csbus 00080014) "   DST_RDY_N: " $(echo ${SRCDST} '/ (2^ 2) % 2' | bc ) " SRC_RDY_N: " $(echo ${SRCDST} '/ (2^ 3) % 2' | bc )
 echo ""
