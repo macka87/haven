f65887840a7b8e0a4a0c7c43537fb87617e0a4ae
Ondra Lengal <ondra.lengal@gmail.com>
2010-09-18 22:17:14 +0000

    Combov2: propagate changes in address space
    git-svn-id: svn+ssh://merlin.fit.vutbr.cz/svn/DPI13@24 bda6da93-004a-4ae9-8e07-715c10848801

--- software/utils/watches.sh
+++ software/utils/watches.sh
@@ -7,6 +7,6 @@ echo ""
 echo "Application core watches:"
 echo "Control register:            " $(csbus 00080000)
 echo ""
-echo "Input interface:    frames:  " $(csbus 00080008) "    invalid frames: " $(csbus 00080010) "   DST_RDY_N: " $(echo ${SRCDST} '/ (2^ 0) % 2' | bc ) " SRC_RDY_N: " $(echo ${SRCDST} '/ (2^ 1) % 2' | bc )
-echo "Output interface:   frames:  " $(csbus 0008000C) "    invalid frames: " $(csbus 00080014) "   DST_RDY_N: " $(echo ${SRCDST} '/ (2^ 2) % 2' | bc ) " SRC_RDY_N: " $(echo ${SRCDST} '/ (2^ 3) % 2' | bc )
+echo "Input interface:    frames:  " $(csbus 00080008) "    invalid frames: " $(csbus 00080010) "   SRC_RDY_N: " $(echo ${SRCDST} '/ (2^ 1) % 2' | bc ) " DST_RDY_N: " $(echo ${SRCDST} '/ (2^ 0) % 2' | bc )
+echo "Output interface:   frames:  " $(csbus 0008000C) "    invalid frames: " $(csbus 00080014) "   SRC_RDY_N: " $(echo ${SRCDST} '/ (2^ 3) % 2' | bc ) " DST_RDY_N: " $(echo ${SRCDST} '/ (2^ 2) % 2' | bc )
 echo ""
