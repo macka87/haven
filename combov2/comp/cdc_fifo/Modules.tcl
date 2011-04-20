02ca8dd16df44d653f37fb84929d36ef48210f58
Ondra Lengal <ondra.lengal@gmail.com>
2011-04-20 07:41:18 +0000

    CDC FIFO: add generated simulation files
    git-svn-id: svn+ssh://merlin.fit.vutbr.cz/svn/DPI13@76 bda6da93-004a-4ae9-8e07-715c10848801

--- combov2/comp/cdc_fifo/Modules.tcl
+++ combov2/comp/cdc_fifo/Modules.tcl
@@ -4,5 +4,9 @@
 # $Id$
 #
 
+set MOD "$MOD $ENTITY_BASE/xilinx_fifos/asfifo_lut_1.vhd"
+set MOD "$MOD $ENTITY_BASE/xilinx_fifos/asfifo_lut_8.vhd"
+set MOD "$MOD $ENTITY_BASE/xilinx_fifos/asfifo_lut_71.vhd"
+
 # Source files
 set MOD "$MOD $ENTITY_BASE/cdc_fifo.vhd"
