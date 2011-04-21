b6c78401e9dfe4fd7f10f04fb21e569e77adfd61
Ondra Lengal <ondra.lengal@gmail.com>
2011-04-21 22:34:06 +0000

    FL_DRIVER_CTRL: add FL_DRIVER controller
    git-svn-id: svn+ssh://merlin.fit.vutbr.cz/svn/DPI13@114 bda6da93-004a-4ae9-8e07-715c10848801

--- combov2/comp/hw_ver/fl_hw_driver/fl_driver_ctrl/Modules.tcl
+++ combov2/comp/hw_ver/fl_hw_driver/fl_driver_ctrl/Modules.tcl
@@ -3,7 +3,7 @@
 # Author(s): Marcela Simkova <xsimko03@stud.fit.vutbr.cz>
 
 # Set paths
-set FIRMWARE_BASE       "$ENTITY_BASE/../../../../../.."
+set FIRMWARE_BASE       "$ENTITY_BASE/../../../.."
 set COMP_BASE           "$FIRMWARE_BASE/comp"
 set BASE_BASE           "$COMP_BASE/base"
 
@@ -14,5 +14,5 @@ set COMPONENTS [list \
 
 # FrameLink Adder of NetCOPE Header 
 if { $ARCHGRP == "FULL" } {
-   set MOD "$MOD $ENTITY_BASE/fl_driver.vhd"
+   set MOD "$MOD $ENTITY_BASE/fl_driver_ctrl.vhd"
 }
