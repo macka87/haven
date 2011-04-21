72ec020018ffdabcaa955fc200aa80ca856280af
Ondra Lengal <ondra.lengal@gmail.com>
2011-04-21 06:05:54 +0000

    Create comp/hw_ver directory and
     * move PRNG_8 there
    
     * add FL_HW_MONITOR
    
    
    git-svn-id: svn+ssh://merlin.fit.vutbr.cz/svn/DPI13@88 bda6da93-004a-4ae9-8e07-715c10848801

---
+++ combov2/comp/hw_ver/fl_hw_monitor/Modules.tcl
@@ -0,0 +1,21 @@
+# Modules.tcl: Local include Modules tcl script
+# Author: Ondrej Lengal <ilengal@fit.vutbr.cz>
+#
+# $Id$
+#
+
+# Set paths
+set COMP_BASE              "$ENTITY_BASE/../.."
+set HW_VER_BASE            "$COMP_BASE/hw_ver"
+set CDC_FIFO_BASE          "$COMP_BASE/cdc_fifo"
+
+set LFSR_PRNG_BASE         "$HW_VER_BASE/lfsr_prng"
+
+# Source files
+set MOD "$MOD $ENTITY_BASE/fl_hw_monitor.vhd"
+
+# Componentss
+set COMPONENTS [list \
+   [ list "CDC_FIFO"       $CDC_FIFO_BASE        "FULL"] \
+   [ list "LFSR_PRNG"      $LFSR_PRNG_BASE       "FULL"] \
+]
