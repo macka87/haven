7476eac409481e051f63c3eac10ffd8e3f3171bc
Ondra Lengal <ondra.lengal@gmail.com>
2011-08-11 07:27:02 +0200

    SIGNAL_OBSERVER: add FRAME_SENDER

--- combov2/comp/hw_ver/signal_observer/Modules.tcl
+++ combov2/comp/hw_ver/signal_observer/Modules.tcl
@@ -13,11 +13,10 @@ set FL_FIFO_BASE           "$FL_BASE/storage/fifo"
 
 
 # Source files
-set MOD "$MOD $ENTITY_BASE/signal_observer.vhd"
-
-# Smart monitor
 set MOD "$MOD $ENTITY_BASE/observer_packetizer.vhd"
 set MOD "$MOD $ENTITY_BASE/frame_sender.vhd"
+
+set MOD "$MOD $ENTITY_BASE/signal_observer.vhd"
 
 # Componentss
 set COMPONENTS [list \
