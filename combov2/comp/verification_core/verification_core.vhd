3812b206977d2fc75c06c5cd206639b37e637788
Ondra Lengal <ondra.lengal@gmail.com>
2011-08-17 18:35:28 +0200

    Verification Core: add the LIMIT_FRAMES generic of SIGNAL_OBSERVER

--- combov2/comp/verification_core/verification_core.vhd
+++ combov2/comp/verification_core/verification_core.vhd
@@ -467,6 +467,7 @@ icon_i : icon3
      IN_DATA_WIDTH   => DUT_DATA_WIDTH,
      OUT_DATA_WIDTH  => ENV_DATA_WIDTH,
      ENDPOINT_ID     => 187,   -- BB hexa
+     LIMIT_FRAMES    => false,
      SEND_X_FRAMES   => 0
    )
    port map(
