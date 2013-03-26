42638d7a7a33390450bce7e92d45fa8e011fb1d7
Martin Funiak <xfunia00@stud.fit.vutbr.cz>
2013-03-26 14:59:19 +0100

    Added files and changes on input side (SW part)

---
+++ codix/fve/env_lib/dpi/sv_dpi_pkg.sv
@@ -0,0 +1,12 @@
+package sv_codix_ca_dpi_pkg;
+
+	import ovm_pkg::*;
+	import sv_codix_ca_param_pkg::*;
+	import sv_codix_ca_seq_pkg::*;
+        import dpi_wrapper_pkg::*;
+
+	`include "ovm_macros.svh"
+	`include "input_wrapper.sv"
+	`include "output_wrapper.sv"
+
+endpackage
